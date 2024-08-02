a8a7c96f86a7fa8daf1dcb3ea7647495d0cf1288
Shadab khan <119776858+shadabBhai@users.noreply.github.com>
2024-08-02 19:31:34 +0530

    define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import Login from "./Pages/Login"
+import Home from "./Pages/Home.page"
+import Product from "./Pages/Product"
+import Thesis from "./Pages/Thesis"
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "/product",
+        element: <Product />
+      },
+      {
+        path: "/thesis",
+        element: <Thesis />
+      }
+    ]
+  }
+])
 const App = () => {
   return (
-    <>
-      <Login />
-    </>
+    <RouterProvider router={router} />
   )
 }
 
