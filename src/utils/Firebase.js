dd558e406fce2d3452b7a7dc6673da98986a3de7
Shadab khan <119776858+shadabBhai@users.noreply.github.com>
2024-08-03 03:05:16 +0530

    add firebse yaml

---
+++ src/utils/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOS4Ms7D7XV-pyw3NFCyIwMB-E5ec1YWo",
+  authDomain: "sahilbookbinding-c02d9.firebaseapp.com",
+  projectId: "sahilbookbinding-c02d9",
+  storageBucket: "sahilbookbinding-c02d9.appspot.com",
+  messagingSenderId: "20730443457",
+  appId: "1:20730443457:web:b9ae6a4c483674eafec640",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
